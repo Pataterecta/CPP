e5f8f014c303b27f618fa55bd604a01a86ac96a0
yookyeoc <yookyeoc@student.42.fr>
2026-04-02 00:25:18 +0200

    hmm

---
+++ CPP05/ex00/Bureaucrat.cpp
@@ -0,0 +1,70 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{
+    if (grade < 1)
+        throw GradeTooHighException();
+    if (grade > 150)
+        throw GradeTooLowException();
+    _grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other)
+{
+    if (this != &other)
+    {
+        // *(const_cast<std::string*>(&_name)) = other._name;
+        this->_grade = other._grade;
+    }
+    return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{   
+}
+
+const std::string& Bureaucrat::getName() const
+{
+    return this->_name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->_grade;
+}
+
+void Bureaucrat::setGrade(int note)
+{
+    if (note > 150)
+        throw GradeTooLowException();
+    else if (note < 1)
+        throw GradeTooHighException();
+    this->_grade = note;
+    
+}
+
+void Bureaucrat::incGrade(int note)
+{
+    if (this->_grade - note < 1)
+        throw GradeTooHighException();
+    _grade = _grade - note;
+}
+
+void Bureaucrat::decGrade(int note)
+{
+    if (this->_grade + note > 150)
+        throw GradeTooLowException();
+    _grade = _grade + note;
+}
+
+std::ostream& operator<<(std::ostream &o, const Bureaucrat &other)
+{
+    o << other.getName() << ", bureaucrat grade " << other.getGrade() << "." << std::endl;
+    return (o);
+}
+// 멤버 연산자면 안돼 왜냐면 왼쪽 피연산자가 그 클래스일 때만 호출되기 때문임!
+// 그래서 이것 땜시 getter 가 필요한거임.. 멤버 함수가 아니라서..
---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat {
+
+    private:
+
+    const std::string _name;
+    int _grade;
+
+
+    public:
+    Bureaucrat(const std::string name, int grade);
+    Bureaucrat(const Bureaucrat &other);
+    Bureaucrat& operator=(const Bureaucrat &other);
+    // std::cout 은 객체(변수), 함수 반환 타입 자리엔 타입이 와야함, 반환도 ostream& 인건 체이닝 때문
+    // 레퍼 안쓰면 진짜 변수가 아닌 복사된 값을 가지고 노는거라 문제 생김
+    // std::cout << b1 << b2; 가능하도록.. 이건 cout 객체에다가 b1을 넣는다임
+    ~Bureaucrat();
+
+    class GradeTooHighException : public std::exception
+    {
+        public: 
+        const char* what() const throw(){
+        return "Grade too high";
+        }
+    };
+    class GradeTooLowException : public std::exception
+    {
+        public:
+        const char* what() const throw(){
+            return "Grade too low";
+        }
+    };
+    const std::string& getName() const;
+    int getGrade() const;
+    void setGrade(int note);
+    void incGrade(int note);
+    void decGrade(int note);
+};
+
+std::ostream& operator<<(std::ostream &o, const Bureaucrat &other);
+
+
+// what 함수는 std::exception 클래스에서 상속 받은 가상 함수(부모에서 이미 virtual임), 예외 객체애 대한 설명을 문자열로 반환
+// const throw : 이 함수가 예외를 던지지 않고 발생시 안전히 호출 될 수 있다 발생 상황 정보 포함
+// https://wo-dbs.tistory.com/159
+
+#endif
