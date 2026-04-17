ce7f8c52bbb76d7db383b5dbf3d4dbb3123b904a
Pataterecta <190707c@gmail.com>
2026-04-17 20:30:43 +0200

    sympa test added

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,11 +1,37 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
+
+extern int cry;
+
 template <typename T>
-void iter(T *array, const int len, T (*func)(void))
+void iter(T *array, const int len, void(*func)(T&)) // im so general
 {
     for (int i = 0; i < len; i++){
-        func()array[i];
+        func(array[i]);
+        // std::cout << "I will change the value when u want" << std::endl; // success :)
+    }
+}
+
+// template <typename T>
+// void iter(T *array, const int len, void(*func)(const T&)) // need to think of its use
+// {
+//     for (int i = 0; i < len; i++){
+//         func(array[i]);
+//         std::cout << "debug to see second f called" << std::endl;
+
+//     }
+// } 
+// 1 converts this cuz const function itself can be used in 1st case
+// compile error ambiguos overloading
+
+template <typename T>
+void iter(const T *array, const int len, void(*func)(const T&)) // no value changing function and value
+{
+     for (int i = 0; i < len; i++){
+        func(array[i]);
+        // std::cout << "debug to see const called" << std::endl; // success :)
     }
 }
 
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,8 +1,43 @@
 #include "iter.hpp"
 
+#include <iostream>
+
+int cry = 0;
+
+void random_cried(int &n)
+{
+    std::cout << "I cried: " << n << " times." << std::endl;
+    std::cout << "idx: " << cry << std::endl;
+    cry++;
+}
+
+void random_cried(const int &n)
+{
+    std::cout << "I const cried: " << n << " times." << std::endl;
+    std::cout << "idx: " << cry << std::endl;
+    cry++;
+}
+
+void zero_waste(int &n)
+{
+    n = 0;
+}
+
 int main(void)
 {
     
-
+    int array[] = {1, 2, 3};
+    const int len = 3;
+    iter(array, len, random_cried);
+    cry = 0;
+    std::cout << "----test-----" << std::endl;
+    const int array2[] = {9, 99, 999, 9999};
+    const int len2 = 4;
+    iter(array2, len2, random_cried); 
+    // overloading test, for both case, uncomment debug const called to see better
+    std::cout << "----test-----" << std::endl;
+    iter(array, len, zero_waste);
+    std::cout << array[0] << ", " << array[1] << " and then " << array[2] << std::endl;
+    // same uncomment the 1st iter function so to check which is called :)
     return (0);
 }
